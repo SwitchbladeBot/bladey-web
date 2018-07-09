fdd59ea58dbe5d1959f048807415db54f3192a25
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-07-09 13:26:00 -0300

    Remove build/ from .gitignore

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
         SWITCHBLADE
