194fc88a991e42c6a705b9e4c854aef458b8b3af
davipatury <davi.patury@yahoo.com.br>
2018-07-10 15:28:46 -0300

    Homepage improvements

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,47 +1,83 @@
 <template>
-  <nav class="navbar ">
+  <nav class="navbar is-transparent is-unselectable" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item wave" href="#">
+      <a class="navbar-item" href="#">
         SWITCHBLADE
       </a>
-
-      <a class="navbar-item is-hidden-desktop" href="https://github.com/jgthms/bulma" target="_blank">
-        <span class="icon" style="color: #333;">
-          <i class="fa fa-github"></i>
-        </span>
+      <a class="navbar-burger" v-on:click="burger" data-target="navMenu" role="button" aria-label="menu" aria-expanded="false">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
       </a>
-
-      <a class="navbar-item is-hidden-desktop" href="https://twitter.com/jgthms" target="_blank">
-        <span class="icon" style="color: #55acee;">
-          <i class="fa fa-twitter"></i>
-        </span>
-      </a>
-
-      <div class="navbar-burger burger" data-target="navMenubd-example">
-          <span></span>
-          <span></span>
-          <span></span>
+    </div>
+    <div class="navbar-menu" id="navMenu">
+      <div class="navbar-end">
+        <div class="navbar-item has-dropdown" id="navDropdown">
+          <a class="navbar-link" v-on:click="dropdown" data-target="navDropdown">
+            <span class="icon user-pic">
+              <img class="round" src="https://cdn.discordapp.com/avatars/135152303773712384/38ea62493b1b22541426b99d835b87ba.png?size=2048" />
+            </span>
+            <span>davipatury</span>
+          </a>
+          <div class="navbar-dropdown is-boxed">
+            <a class="navbar-item">
+              <span class="icon">
+                <fai icon="tachometer-alt" />
+              </span>
+              <span>Dashboard</span>
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              <span class="icon">
+                <fai icon="sign-out-alt" />
+              </span>
+              <span>Logout</span>
+            </a>
+          </div>
         </div>
       </div>
+    </div>
   </nav>
 </template>
 
 <script>
 export default {
-  name: 'navbar'
+  name: 'navbar',
+  methods: {
+    burger: function (event) {
+      const $burger = event.target
+      const $target = document.getElementById($burger.dataset.target)
+      $burger.classList.toggle('is-active')
+      $target.classList.toggle('is-active')
+    },
+    dropdown: function (event) {
+      const $dropdown = event.target
+      const $target = document.getElementById($dropdown.dataset.target)
+      $target.classList.toggle('is-active')
+    }
+  }
 }
 </script>
 
 <style scoped>
-  @import url('https://fonts.googleapis.com/css?family=Montserrat:900i,400');
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600,900,900i');
 
-  .navbar-brand {
+  .navbar-brand > .navbar-item, .navbar-burger {
     font-family: 'Montserrat', sans-serif;
     font-weight: 900;
     font-style: italic;
     color: white;
   }
-  .navbar-brand a:hover {
+
+  .navbar-brand > .navbar-item:hover, .navbar-burger:hover {
     color: white;
+  }
+
+  .user-pic {
+    margin-right: 0.5rem;
+  }
+
+  .round {
+    border-radius: 100%;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCode, faEnvelope, faLifeRing, faSignInAlt, faSignOutAlt, faTachometerAlt } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(faCode, faEnvelope, faLifeRing, faSignInAlt, faSignOutAlt, faTachometerAlt)
+
+Vue.component('fai', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
