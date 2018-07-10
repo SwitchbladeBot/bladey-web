2c9ce3cd485c324012bb4784e3913e60e1ba70db
kaikecarlos <caheasta@gmail.com>
2018-07-10 11:41:29 -0300

    Fix navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,28 @@
 <template>
-  <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
-    <div class="container">
-      <div class="navbar-brand">
+  <nav class="navbar ">
+    <div class="navbar-brand">
+      <a class="navbar-item wave" href="#">
         SWITCHBLADE
+      </a>
+
+      <a class="navbar-item is-hidden-desktop" href="https://github.com/jgthms/bulma" target="_blank">
+        <span class="icon" style="color: #333;">
+          <i class="fa fa-github"></i>
+        </span>
+      </a>
+
+      <a class="navbar-item is-hidden-desktop" href="https://twitter.com/jgthms" target="_blank">
+        <span class="icon" style="color: #55acee;">
+          <i class="fa fa-twitter"></i>
+        </span>
+      </a>
+
+      <div class="navbar-burger burger" data-target="navMenubd-example">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <div class="navbar-menu">
-        <!-- navbar start, navbar end -->
-      </div>
-    </div>
   </nav>
 </template>
 
@@ -24,5 +39,9 @@ export default {
     font-family: 'Montserrat', sans-serif;
     font-weight: 900;
     font-style: italic;
+    color: white;
+  }
+  .navbar-brand a:hover {
+    color: white;
   }
 </style>
