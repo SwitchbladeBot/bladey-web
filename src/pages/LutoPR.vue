e49b61d18599b6e3d2d3466c7dd6a2f9662146ce
Doges <xdoges@idonthave.life>
2019-01-06 21:25:47 -0200

    LUTO PR

---
+++ src/pages/LutoPR.vue
@@ -0,0 +1,4 @@
+<template>
+      <img draggable="false" src="https://cdn.discordapp.com/attachments/318165772549488640/518918094127890452/unknown.png">
+      <h1 class="title has-text-white">LUTO PR</h1>
+</template>
