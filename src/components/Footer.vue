3767c2848c54c6f76c9d0a331a15a33c9d92849b
David Paiva <pillgp@icloud.com>
2019-01-31 14:04:55 +0000

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer has-text-white">
   <div class="content has-text-centered">
     <p>
-    Made with <b-icon class="heart" icon="heart" custom-size="mdi-18px" /> by <router-link class="colorFade" to="/contributors">a lot of people</router-link>, powered by <a class="colorFade" href="https://www.vultr.com/">Vultr</a> and <a class="colorFade" href="https://vuejs.org/">Vue.js</a>.<br />
+    Made with <b-icon class="heart" icon="heart" custom-size="mdi-18px" /> by <router-link class="colorFade" to="/contributors">a lot of people</router-link>, powered by <a class="colorFade" href="https://www.vultr.com/">Vultr</a>, <a class="colorFade" href="https://vuejs.org/">Vue.js</a> and <a class="colorFade" href="https://www.netlify.com/">Netlify</a>.<br />
     "Discord", "DiscordApp" and any associated logos are registered trademarks of Discord Inc.
     </p>
   </div>
