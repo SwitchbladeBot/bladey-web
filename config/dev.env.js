17d103ba359865153926338c31a410b0227be682
Matheus Dias <methidezeeni@gmail.com>
2019-03-23 21:22:39 -0300

    nvm

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   CLIENT_ID: '"346014632395407362"',
   REDIRECT_URI: '"http://localhost:8080/auth"',
-  BLADEY_API_ROOT: '"https://dev.switchblade.xyz/api"'
+  BLADEY_API_ROOT: '"http://localhost:8000/api"'
 })
