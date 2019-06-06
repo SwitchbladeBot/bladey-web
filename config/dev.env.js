03db5337d288f8dfd9f5867a35d399e1110da636
PillGP <pillgp@icloud.com>
2019-06-06 06:00:12 -0400

    oh fuck

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,7 +3,7 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  CLIENT_ID: '"515607503648718868"',
-  REDIRECT_URI: '"http://bladey-web-daavid665137575.codeanyapp.com:3000/auth"',
-  BLADEY_API_ROOT: '"https://1grtt7sz.apps.lair.io/api"'
+  CLIENT_ID: '"346014632395407362"',
+  REDIRECT_URI: '"http://localhost:8080/auth"',
+  BLADEY_API_ROOT: '"http://localhost:8000/api"'
 })
